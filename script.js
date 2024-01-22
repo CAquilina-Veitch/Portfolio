347b884e9bda2f542ada4d78e68707e3e7d5b2dd
Cody <1027166@student.sae.edu.au>
2024-01-22 12:53:57 +0800

    2

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
   })(jQuery);
 
   // input text for typing animation
-  $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
+  $("#holder").writeText("Games Programmer and Designer");
 
   // initialize wow.js
   new WOW().init();
